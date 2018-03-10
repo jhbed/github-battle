071134dcc9a62a570c1701ebd45f2423f981650c
jhbed <jakehbedard@gmail.com>
2018-03-10 13:21:53 -0800

    added AJAX request functionality using axios. Need to study how axios is working here

--- src/components/Popular.js
+++ src/components/Popular.js
@@ -1,12 +1,46 @@
 import React, { Component } from 'react';
 const PropTypes = require('prop-types');
+const api = require('../utils/api');
 
+
+//STATELESS FUNCTIONAL COMPONENT
+/*
+Something weird: The list of languages is stored here is this stateless component,
+however, one of these languages is going to be stored in our parent's state. To me,
+it seems more readable/intuitive to store this list in parent, however, Tyler seems
+to want it here. 
+*/
+function SelectLang(props){
+	const languages = ['All', 'Javascript', 'Ruby','Java','CSS','Python'];
+	return (
+		<ul className="languages">
+			{languages.map(language => 
+				<li 
+					style={language === props.selectedLanguage ? {color: 'blue'} : null}
+					onClick={props.onSelect.bind(null, language)} //UPDATES PARENT STATE
+					key={language}>
+					{language}
+				</li>
+			)}
+		</ul>
+	);
+}
+
+SelectLang.propTypes = {
+	selectedLanguage: PropTypes.string.isRequired,
+	onSelect: PropTypes.func.isRequired,
+}
+
+
+
+//REGULAR COMPONENT
 class Popular extends React.Component {
 
 	constructor(props){
 		super(props);
 		this.state = {
 			selectedLanguage: 'All',
+			repos: null,
 		}
 
 		this.updateLanguage = this.updateLanguage.bind(this);
@@ -15,34 +49,43 @@ class Popular extends React.Component {
 	updateLanguage(lang){
 		this.setState(function(){
 			return ({ 
-				selectedLanguage: lang 
+				selectedLanguage: lang,
+				repos: null, 
 			});
 		});
+
+
+		api.fetchPopularRepos(lang)
+			.then(function(repos){
+				this.setState(function(){
+					return({
+						repos: repos,
+					})
+				})
+			}.bind(this));
+	}
+
+	componentDidUpdate(){
+		console.log("State updated to: " + this.state.selectedLanguage);
+	}
+
+	componentDidMount(){
+		this.updateLanguage(this.state.selectedLanguage);
 	}
 
 
   render() {
-  	let languages = ['All', 'Javascript', 'Ruby','Java','CSS','Python'];
     return (
-    	/*
-		Notice the arrow function** 
-		Since we have a function there, it is important to bind 'this' t
-		to the object, so it is not bound to the arrow function. We do this 
-		binding in the constructor.
-    	*/
-    	<ul className="languages">
-    		{languages.map(language => 
-    			<li 
-    				style={language === this.state.selectedLanguage ? {color: 'blue'} : null}
-    				onClick={this.updateLanguage.bind(null, language)}
-    				key={language}>
-    				{language}
-    			</li>
-    		,this)}
-    	</ul>
-
+    	<div>
+    		<SelectLang 
+			selectedLanguage = {this.state.selectedLanguage}
+			onSelect = {this.updateLanguage}
+    		/>
+    		{JSON.stringify(this.state.repos, null, 2)}
+    	</div>
     );
   }
 }
 
+//we only need to export Popular because Popular is rendering SelectLang, which then gets exported.
 export default Popular;
